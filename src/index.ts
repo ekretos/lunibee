c5c02e81c1a9b67039c532494d1520ee7f853be6
ErrorDoc404 <akashsinhakvs@gmail.com>
2026-08-27 14:14:14 +0530

    Add public API

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { Client } from "./client.js";
+export { GatewayIntentBits } from "./gateway/intents.js";
+export { LunibeeError, RESTError, GatewayError } from "./errors.js";
+export type { ClientOptions, ClientUser, GatewayPayload } from "./types.js";
