fc825eb325bd31e1a15d92aa00d89179f785a361
ErrorDoc404 <akashsinhakvs@gmail.com>
2026-08-27 14:54:03 +0530

    Export interactions from structures

--- packages/structures/src/index.ts
+++ packages/structures/src/index.ts
@@ -1,9 +1,7 @@
 /** Common Discord snowflake-backed structure. */
 export class BaseStructure {
-    /** Discord resource ID. */
-    public readonly id: string;
-    /** Creates a structure from an ID. */
-    public constructor(id: string) { this.id = id; }
+    /** Discord resource ID. */ public readonly id: string;
+    /** Creates a structure from an ID. */ public constructor(id: string) { this.id = id; }
 }
 
 /** Represents a Discord user. */
@@ -13,23 +11,19 @@ export class User extends BaseStructure {
     /** Avatar hash. */ public avatar: string | null;
     /** Whether the user is a bot. */ public bot: boolean;
     /** Creates a user from Discord data. */
-    public constructor(data: { id: string; username: string; global_name?: string | null; avatar?: string | null; bot?: boolean }) {
-        super(data.id); this.username = data.username; this.globalName = data.global_name ?? null; this.avatar = data.avatar ?? null; this.bot = data.bot ?? false;
-    }
+    public constructor(data: { id: string; username: string; global_name?: string | null; avatar?: string | null; bot?: boolean }) { super(data.id); this.username = data.username; this.globalName = data.global_name ?? null; this.avatar = data.avatar ?? null; this.bot = data.bot ?? false; }
 }
 
 /** Represents a Discord guild. */
 export class Guild extends BaseStructure {
     /** Guild name. */ public name: string;
-    /** Creates a guild from Discord data. */
-    public constructor(data: { id: string; name: string }) { super(data.id); this.name = data.name; }
+    /** Creates a guild from Discord data. */ public constructor(data: { id: string; name: string }) { super(data.id); this.name = data.name; }
 }
 
 /** Represents a Discord channel. */
 export class Channel extends BaseStructure {
     /** Discord channel type. */ public type: number;
-    /** Creates a channel from Discord data. */
-    public constructor(data: { id: string; type: number }) { super(data.id); this.type = data.type; }
+    /** Creates a channel from Discord data. */ public constructor(data: { id: string; type: number }) { super(data.id); this.type = data.type; }
 }
 
 /** Represents a Discord message. */
@@ -38,9 +32,8 @@ export class Message extends BaseStructure {
     /** Message author. */ public author: User;
     /** Channel containing the message. */ public channelId: string;
     /** Creates a message from Discord data. */
-    public constructor(data: { id: string; content: string; author: User | { id: string; username: string; global_name?: string | null; avatar?: string | null; bot?: boolean }; channel_id: string }) {
-        super(data.id); this.content = data.content; this.author = data.author instanceof User ? data.author : new User(data.author); this.channelId = data.channel_id;
-    }
+    public constructor(data: { id: string; content: string; author: User | { id: string; username: string; global_name?: string | null; avatar?: string | null; bot?: boolean }; channel_id: string }) { super(data.id); this.content = data.content; this.author = data.author instanceof User ? data.author : new User(data.author); this.channelId = data.channel_id; }
 }
 
 export { GuildMember, Role, TextChannel } from "./resources.js";
+export * from "./interactions.js";
