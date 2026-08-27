8562caa87d5df15ecaa4b49d011749cf566bea46
ErrorDoc404 <akashsinhakvs@gmail.com>
2026-08-27 14:16:46 +0530

    Export core clients

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export { Client } from "./client.js";
+export { REST } from "./rest.js";
 export { GatewayIntentBits } from "./gateway/intents.js";
+export { Shard } from "./gateway/shard.js";
+export { ShardManager } from "./gateway/sharding.js";
 export { LunibeeError, RESTError, GatewayError } from "./errors.js";
-export type { ClientOptions, ClientUser, GatewayPayload } from "./types.js";
+export type { ClientOptions, ClientUser, GatewayOptions, GatewayPayload, RESTOptions } from "./types.js";
