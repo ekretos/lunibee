4122972eef36f0ff7532cfaee6cf429504b77af6
ErrorDoc404 <akashsinhakvs@gmail.com>
2026-08-29 12:58:33 +0530

    feat(builders): export modal and entity select builders

--- packages/builders/src/index.ts
+++ packages/builders/src/index.ts
@@ -1,4 +1,14 @@
 export { EmbedBuilder } from "./embed.js";
 export type { EmbedField } from "./embed.js";
-export { ButtonBuilder, ButtonStyle, ComponentType, ActionRowBuilder, StringSelectBuilder } from "./components.js";
+export {
+    ButtonBuilder,
+    ButtonStyle,
+    ComponentType,
+    ActionRowBuilder,
+    StringSelectBuilder,
+    EntitySelectBuilder,
+    ModalBuilder,
+    TextInputBuilder,
+    TextInputStyle
+} from "./components.js";
 export { SlashCommandBuilder, StringOptionBuilder } from "./commands.js";
