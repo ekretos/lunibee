6a97bb5efcda0d51c670d15be20ba71261566ff3
ErrorDoc404 <akashsinhakvs@gmail.com>
2026-08-29 12:27:58 +0530

    feat(builders): export typed command option builders

--- packages/builders/src/index.ts
+++ packages/builders/src/index.ts
@@ -2,3 +2,4 @@ export { EmbedBuilder } from "./embed.js";
 export type { EmbedField } from "./embed.js";
 export { ButtonBuilder, ButtonStyle, ComponentType, ActionRowBuilder, StringSelectBuilder } from "./components.js";
 export { SlashCommandBuilder, StringOptionBuilder } from "./commands.js";
+export { BaseOptionBuilder, NumberOptionBuilder, BooleanOptionBuilder, UserOptionBuilder, ChannelOptionBuilder, RoleOptionBuilder, MentionableOptionBuilder } from "./options.js";
